dcb61541c65d23ca51c436f18758acc416c2237d
shantanu <shanu09.sm@gmail.com>
2020-11-11 12:27:02 +0530

    story-bookcompleted

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const path = require('path')
 const passport = require('passport')
 const exphbs=require('express-handlebars')
 const connectDB=require('./config/db')
-const session=require('express-session')
-
+const session = require('express-session')
+const MongoStore=require('connect-mongo')(session)
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const { format } = require('path')
 //load config
 dotenv.config({path:'./config/config.env'})
 
@@ -17,36 +20,62 @@ connectDB()
 
 const app=express()
 
-//Logging
-if (process.env.NODE_ENV === 'development'){
-app.use(morgan('dev'))
-}
+//body parser
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+//mthod override
+app.use(
+    methodOverride(function (req, res) {
+      if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        let method = req.body._method
+        delete req.body._method
+        return method
+      }
+    })
+  )
+
+//handelbars helpers
+const {formatDate,stripTags,truncate,editIcon,select}=require('./helpers/hbs')
 
 //handelbars
-app.engine('.hbs', exphbs({defaultLayout:'main',extname:'.hbs'}))
+app.engine('.hbs', exphbs({
+    helpers:{
+        formatDate,
+        stripTags,
+        truncate,
+        editIcon,
+        select,
+    },defaultLayout:'main',extname:'.hbs'}))
 app.set('view engine','.hbs')
 
-//sessions
+//Session
 app.use(session({
     secret:'keyboard cat',
-    resave: false,
+    resave:false,
     saveUninitialized:false,
+    store:new MongoStore({mongooseConnection:mongoose.connection})
 }))
 
 //passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
 
+//set global var
+app.use(function (req, res,next){
+res.locals.user=req.user || null
+next()
+})
+
 //static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 //routes
-const route=require('./route/index')
-app.use('/',route)
-const groute=require('./route/auth')
-app.use('/auth',groute)
+app.use('/', require('./route/index'))
+app.use('/auth', require('./route/auth'))
+app.use('/stories', require('./route/stories'))
 
 
 const PORT=process.env.PORT||5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port${PORT} `))
+app.listen(PORT, console.log(`Server running on port${PORT} `))
--- route/index.js
+++ route/index.js
@@ -1,18 +1,35 @@
 const express = require('express')
 const router = express.Router()
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
+const Story=require('../models/Story')
 
 // @desc login / landing page
 // @route GET/
-router.get('/',(req,res)=>{
-    res.render('login',{
-        layout:'login'    
+router.get('/', ensureGuest,(req, res) => {
+    res.render('login', {
+      layout: 'login',
     })
-})
+  })
 
 // @desc DashBoard
 // @router GET /dashboard
-router.get('/dashboard',(req,res)=>{
-    res.render('dashboard')
-})
+router.get('/dashboard',ensureAuth, async(req, res) => {
+
+  try{
+    //inorder to pass value in a handelbar template use .lean
+    const stories=await Story.find({user:req.user.id}).lean()
+    res.render('dashboard',{
+      name:req.user.firstName,
+      stories
+    })
+
+  }catch(err){
+    //handel err cuz i dont have privalage like react who provide jason 
+    console.log(err)
+    res.render('error/500')
+  }
+
+      
+    })
 
 module.exports = router
